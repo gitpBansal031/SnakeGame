9df6e25c14c26b7baea50abc279a7ea07e93b76f
gitpBansal031 <bansalpulkit3103@gmail.com>
2025-04-11 15:30:18 +0530

    added music and css

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 let board=document.querySelector(".board");
 let score=document.querySelector(".score");
-let dir={x:0,y:-1};
+let startmusic=new Audio("music/music.mp3");
+let move=new Audio("music/move.mp3");
+let foodmusic=new Audio("music/food.mp3");
+let gameovermusic=new Audio("music/gameover.mp3");
+let dir;
 let prevTime=0;
 let snakeArr=[];
-let food={x:6,y:7};
-let currAxis=0;
-let gameStart=0;
-let count=0;
+let food;
+let currAxis;
+let gameStart;
+let count;
 
 function main(ctime){
     if(gameStart==0){
@@ -32,6 +36,7 @@ function checkGameOver(){
     let status=0;
     for(let i=1;i<snakeArr.length;i++){
         if(snakeArr[i].x==snakeArr[0].x && snakeArr[i].y==snakeArr[0].y){
+            gameovermusic.play();
             alert(`Game Over! Your total score is : ${count}`);
             status=1;
             break;
@@ -58,8 +63,9 @@ function isFoodEaten(){
             food.y=-1;
         }
         count++;
+        foodmusic.play();
         score.innerHTML=`Your score is : ${count}`;
-        console.log("Food eaten");
+        // console.log("Food eaten");
     }
 }
 
@@ -69,6 +75,7 @@ function changeSnakePos(){
     let newHead={x:currHead.x+dir.x,y:currHead.y+dir.y};
     let status=0;
     if(isCollide(newHead)){
+        gameovermusic.play();
         alert(`Game Over! Your total score is : ${count}`);
         status=1;
     }
@@ -108,43 +115,40 @@ function displaySnake(){
 
 window.addEventListener("keydown",(e)=>{
     if(gameStart==0){
+        startmusic.pause();
         gameStart=1;
         score.innerHTML=`Your score is : 0`;
         window.requestAnimationFrame(main);
     }else{
-        let newAxis=currAxis;
+        move.play();
         if(e.key=="ArrowUp" && currAxis!=0){
-            console.log("a");
             dir.x=0;
             dir.y=-1;
-            newAxis=0;
+            currAxis=0;
         }else if(e.key=="ArrowDown" && currAxis!=0){
-            console.log("b");
             dir.x=0;
             dir.y=1;
-            newAxis=0;
+            currAxis=0;
         }else if(e.key=="ArrowLeft" && currAxis!=1){
-            console.log("c");
             dir.x=-1;
             dir.y=0;
-            newAxis=1;
+            currAxis=1;
         }else if(e.key=="ArrowRight" && currAxis!=1){
-            console.log("d");
             dir.x=1;
             dir.y=0;
-            newAxis=1;
+            currAxis=1;
         }
-        currAxis=newAxis;
-        console.log("end");
+        // console.log("end");
     }
 })
 function startGame(){
+    // startmusic.play();
     gameStart=0;
     snakeArr=[{x:9,y:9},{x:9,y:10}];
     dir={x:0,y:-1};
     food={};
     food.x=Math.floor(Math.random()*18)+1;
-    food.y=Math.floor(Math.random()*18)+1;    
+    food.y=Math.floor(Math.random()*18)+1;
     currAxis=0;
     count=0;
     score.innerHTML=`Press any key to start the game`;
