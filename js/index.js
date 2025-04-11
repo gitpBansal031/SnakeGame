d4e2a6405802fa35a5e3ae8dcfefda45b929965b
gitpBansal031 <bansalpulkit3103@gmail.com>
2025-04-11 11:06:35 +0530

    display snake

--- js/index.js
+++ js/index.js
@@ -1,7 +1,40 @@
 let dir={x:0,y:0};
 let prev=0;
-let body=document.querySelector(".body");
+let board=document.querySelector(".board");
+let snakeArr=[{x:13,y:14},{x:13,y:15},{x:13,y:16}];
+let food={x:6,y:7};
+
 function main(ctime){
-    // window.requestAnimationFrame(main);
+    displaySnake();
 }
-window.requestAnimationFrame(main);
+function displaySnake(){
+    board.innerHtml="";
+    snakeArr.forEach((e,index)=>{
+        let snakeElement=document.createElement("div");
+        snakeElement.style.gridRowStart=e.y;
+        snakeElement.style.gridColumnStart=e.x;
+        if(index==0){
+            snakeElement.classList.add("head");
+        }else{
+            snakeElement.classList.add("body");
+        }
+        board.appendChild(snakeElement);
+    })
+}
+
+window.addEventListener("keydown",(e)=>{
+    if(e.key=="ArrowUp"){
+        dir.x=0;
+        dir.y=-1;
+    }else if(e.key=="ArrowDown"){
+        dir.x=0;
+        dir.y=1;
+    }else if(e.key=="ArrowLeft"){
+        dir.x=-1;
+        dir.y=0;
+    }else if(e.key=="ArrowRight"){
+        dir.x=1;
+        dir.y=0;
+    }
+    window.requestAnimationFrame(main);
+})
